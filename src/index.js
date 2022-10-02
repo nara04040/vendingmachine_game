f1ff66fa8282b22f91db598f5e8bb2fa1b03d6c5
nara04040 <nara04040@naver.com>
2022-10-02 23:57:10 +0900

    feat: 상품추가 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import { DOM_SELECTOR } from './constants';
 import renderAppTemplate from './renderAppTemplate';
 import renderProductAddTemplate from './renderProductAddTemplate';
+import attachProductAddEvent from './attachProductAddEvent';
+import Product from './product';
 
 renderAppTemplate();
+
+const product = new Product();
 
 const $productAddMenu = document.getElementById(DOM_SELECTOR.productAddMenu);
 $productAddMenu.addEventListener('click', () => {
   renderProductAddTemplate(); // 이 함수 필요
+  attachProductAddEvent(product);
 });
